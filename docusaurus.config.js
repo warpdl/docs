a40130eaa7e19d7bf8635ab3e564b88960eaccd4
anonyindian <anonyindian1@gmail.com>
2023-06-08 19:07:37 +0530

    update repository links in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'warpdl', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/warpdl/docs/tree/master/',
+            'https://github.com/warpdl/website/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/warpdl/docs/tree/master/',
+            'https://github.com/warpdl/website/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
